f50b1db372b88c186541f6904652f16f3816f64d
ilies ayachi <iliesaya@gmail.com>
2021-11-02 11:26:02 +0300

    first modif for citiesos energy

---
+++ draft_ydata-synthetic.py
@@ -0,0 +1,10 @@
+#%%
+from os import path
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from ydata_synthetic.synthesizers import ModelParameters
+from ydata_synthetic.preprocessing.timeseries import processed_stock
+from ydata_synthetic.synthesizers.timeseries import TimeGAN
+#%%
